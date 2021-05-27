b12cde24c13a49edc58e8677f2f8ff28ee756e0a
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-05-27 18:00:00 -0400

    Fix linebreaks

--- okta-hosted-login/src/config.js
+++ okta-hosted-login/src/config.js
@@ -1,8 +1,6 @@
 const CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
-const ISSUER =
-  process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
-const OKTA_TESTING_DISABLEHTTPSCHECK =
-  process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+const ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
+const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 const REDIRECT_URI = `${window.location.origin}/callback`;
 
 export default {
